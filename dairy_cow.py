616060a300300247231713308a94a93fca12111d
luolongfei <mybsdc@gmail.com>
2020-06-30 10:56:15 +0800

    update

--- dairy_cow.py
+++ dairy_cow.py
@@ -47,9 +47,8 @@ def catch_exception(origin_func):
             print(f'请求超时：{self.driver.current_url}')
         except UserWarning as uw:
             print('警告：{}'.format(str(uw)))
-        except WebDriverException:
-            print('未知错误，可能是 chromedriver 与本地谷歌无头浏览器版本不匹配，可检查并前往 https://chromedriver.chromium.org/downloads '
-                  '下载匹配的版本，当然也可能不是这个原因:)')
+        except WebDriverException as wde:
+            print(f'未知错误：{str(wde)}')
         except Exception as e:
             print('出错：{} 位置：{}'.format(str(e), traceback.format_exc()))
         finally:
@@ -81,10 +80,11 @@ class DairyCow(object):
         self.options.add_argument('--profile-directory=Default')
         self.options.add_argument('--incognito')  # 隐身模式
         self.options.add_argument('--disable-plugins-discovery')
+        self.options.add_argument('--log-level=3')  # 关闭浏览器 console 输出
         self.options.add_argument('--start-maximized')
         # self.options.add_argument('--window-size=1366,768')
 
-        # self.options.add_argument('--headless')
+        self.options.add_argument('--headless')
         self.options.add_argument('--disable-gpu')  # 谷歌官方文档说加上此参数可减少 bug，仅适用于 Windows 系统
 
         # 解决 unknown error: DevToolsActivePort file doesn't exist
@@ -92,7 +92,7 @@ class DairyCow(object):
         self.options.add_argument('--disable-dev-shm-usage')  # 解决资源限制，仅适用于 Linux 系统
 
         self.driver = webdriver.Chrome(executable_path=os.getenv('EXECUTABLE_PATH'), options=self.options)
-        self.driver.implicitly_wait(DairyCow.timeout)
+        self.driver.implicitly_wait(DairyCow.timeout)  # 隐式等待
 
         # 防止通过 window.navigator.webdriver === true 检测模拟浏览器
         # 参考：
@@ -106,14 +106,16 @@ class DairyCow(object):
             """
         })
 
-        # 统配显式等待
-        self.wait = WebDriverWait(self.driver, timeout=DairyCow.timeout, poll_frequency=0.5)
+        # 自定义显式等待
+        self.wait = self.__driver_wait
 
         self.cookies_file = 'cookies_jar'
-        self.cookies = None
 
         self.username = os.getenv('USERNAME')
         self.password = os.getenv('PASSWORD')
+
+    def __driver_wait(self, timeout=None):
+        return WebDriverWait(self.driver, timeout=DairyCow.timeout if timeout is None else timeout, poll_frequency=0.5)
 
     def __login(self, force=False):
         """
@@ -124,9 +126,11 @@ class DairyCow(object):
         if not force and os.path.exists(self.cookies_file):
             print('发现已存在 cookies 文件，免登录')
             with open(self.cookies_file, 'rb') as f:
-                self.cookies = pickle.load(f)
+                cookies = pickle.load(f)
+                for cookie in cookies:
+                    self.driver.add_cookie(cookie)
 
-                return self.cookies
+                return cookies
 
         self.driver.get(DairyCow.login_url)
 
@@ -143,32 +147,45 @@ class DairyCow(object):
 
         self.__fuck_captcha()
 
-        # self.driver.find_element_by_class_name('password-login').click()
-
-        cookies = {}
-        for cookie in self.driver.get_cookies():
-            cookies[cookie['name']] = cookie['value']
+        self.driver.find_element_by_class_name('password-login').click()
+        time.sleep(5)  # 跳转需要时间
 
         # cookies 持久化
-        with open(self.cookies_file, 'wb') as f:
+        t = self.driver.get_cookies()
+        cookies = [{'name': cookie['name'], 'value': cookie['value']} for cookie in self.driver.get_cookies()]
+        with open(self.cookies_file, 'wb') as f:  # b 参数声明以二进制模式，不用也不能指定编码
             pickle.dump(cookies, f)
 
-        self.cookies = cookies
-
-        return self.cookies
+        return cookies
 
     def __fuck_captcha(self, max_retry_num=6):
         time.sleep(0.2)
         captcha_slider = EC.visibility_of_element_located((By.ID, 'nc_1_n1z'))(self.driver)
 
         if captcha_slider:
-            ActionChains(self.driver).click_and_hold(on_element=captcha_slider).perform()
-            ActionChains(self.driver).move_by_offset(xoffset=28, yoffset=0).perform()
-            ActionChains(self.driver).release(on_element=captcha_slider).perform()
+            for num in range(1, max_retry_num + 1):
+                ActionChains(self.driver).click_and_hold(on_element=captcha_slider).perform()
+                ActionChains(self.driver).move_by_offset(xoffset=258, yoffset=0).perform()
+                ActionChains(self.driver).release(on_element=captcha_slider).perform()
 
-            # success_icon = EC.presence_of_element_located((By.CLASS_NAME, 'btn_ok'))(self.driver)
-            pass
+                try:
+                    # 根据绿勾图标是否可见判断验证通过与否
+                    self.wait(0.2).until(EC.visibility_of_element_located((By.CLASS_NAME, 'btn_ok')))
+                    print('已通过滑动验证')
 
+                    return True
+                except TimeoutException as te:
+                    print(f'滑块验证未通过，将重试：{str(te)}')
+                    DairyCow.row_print(f'开始第 {num} 次尝试...')
+                    captcha_reset_btn = self.wait(0.2).until(
+                        EC.element_to_be_clickable((By.XPATH, '//*[@id="nocaptcha-password"]/div/span/a')))
+                    captcha_reset_btn.click()
+
+                    time.sleep(0.4)
+
+            raise UserWarning(f'滑块验证不通过，共尝试{max_retry_num}次')
+
+        return True
 
     @staticmethod
     def row_print(string):
